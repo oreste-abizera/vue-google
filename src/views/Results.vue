50ad569776be843eca1ca69d1055c5fee9642fa1
oreste-abizera <oresteabizera11@gmail.com>
2021-09-26 11:58:45 +0200

    App init

---
+++ src/views/Results.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    Results page
+  </div>
+</template>
+<script>
+// import ResultPage from "@/components/ResultPage.vue";
+
+export default {
+  components: {
+    // ResultPage,
+  },
+};
+</script>
+<style scoped></style>
